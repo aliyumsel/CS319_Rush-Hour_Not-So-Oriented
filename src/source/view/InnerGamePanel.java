71fd0d37d56c7a6bb6f50e07094e9cc3e98c01ab
Deniz Dalkilic <denizdalkilic1998@gmail.com>
2018-11-20 21:15:26 +0300

    Pop up window added (End of Level)

--- src/source/view/InnerGamePanel.java
+++ src/source/view/InnerGamePanel.java
@@ -16,38 +16,57 @@ import source.controller.Input;
 import source.model.Map;
 
 @SuppressWarnings("serial")
-public class InnerGamePanel extends JPanel
-{
+public class InnerGamePanel extends JPanel {
+	private GuiPanelManager guiPanelManager;
 	private Map map;
+	private EndOfLevelPanel endOfLevelPanel;
 
-	public InnerGamePanel() throws FileNotFoundException
-   {
-		setPreferredSize(new Dimension(450,450));
+	public InnerGamePanel(GuiPanelManager guiManager) throws FileNotFoundException {
+		this.guiPanelManager = guiManager;
+		setPreferredSize(new Dimension(450, 450));
+		createEndOfLevelPanel();
+		
+		
+		
+		setVisible(true);
+		//setOpaque(false);
+
 	}
 
-	void updatePanel()
-	{
-	   if (!isShowing())
-      {
-         return;
-      }
-      map = GameEngine.instance.mapController.getMap();
-      //vehicleArray = map.getVehicleArray();
-      repaint();
+	void updatePanel() {
+		if (!isShowing()) {
+			return;
+		}
+		map = GameEngine.instance.mapController.getMap();
+		endOfLevelPanel.updatePanel();
+		// vehicleArray = map.getVehicleArray();
+		repaint();
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		setBackground(Color.WHITE);
 
-		if (map == null)
-      {
-         return;
-      }
+		if (map == null) {
+			return;
+		}
 
-		for (Vehicle vehicle : map.getVehicleArray())
-		{
+		for (Vehicle vehicle : map.getVehicleArray()) {
 			vehicle.draw(g);
 		}
+	}
+
+	public void setEndOfLevelPanelVisible(boolean bool) {
+		endOfLevelPanel.setVisible(bool);
+	}
+
+	private void createEndOfLevelPanel() {
+
+		endOfLevelPanel = new EndOfLevelPanel(guiPanelManager);
+		add(endOfLevelPanel);
+		endOfLevelPanel.setVisible(false);
+		Insets insets = getInsets();
+		Dimension size = endOfLevelPanel.getPreferredSize();
+		//endOfLevelPanel.setBounds(50 + insets.left, 100 + insets.top, size.width, size.height);
 	}
 }
